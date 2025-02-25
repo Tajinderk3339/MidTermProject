d5d4d799493f592d5ac7a7f5ef504e14975324ba
TajinderK3339 <tajinderkumar001@algomau.ca>
2025-02-25 17:19:50 -0500

    last

--- js/script.js
+++ js/script.js
@@ -16,6 +16,11 @@ function getFact(){
 
 function lightFunction(){
     var element=document.getElementsByTagName('nav')
-    element.setAttribute('style', 'color: red; margin: 10px;');
+    element.setAttribute('style', 'color: white; margin: 10px;');
+    document.body.setAttribute('style','background-color:white')
+}
+function darkFunction(){
+    var element=document.getElementsByTagName('nav')
+    element.setAttribute('style', 'color: black; margin: 10px;');
     document.body.setAttribute('style','background-color:white')
 }
