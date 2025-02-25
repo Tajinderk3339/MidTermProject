7e9ca4c2fc704c3227922afda05fd8b982c9fcec
TajinderK3339 <tajinderkumar001@algomau.ca>
2025-02-25 17:15:51 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let fact=(a)=>{
+    let m=1;
+   for(let i=a;i>=1;i--){
+    m=m*i;
+   }
+   return m;
+}
+
+function getFact(){
+    var text=document.getElementById('name').value;
+    // console.log(text);
+    let factorial=fact(text);
+    var output=document.getElementById('answer');
+    output.value=factorial;
+}
+
+function lightFunction(){
+    var element=document.getElementsByTagName('nav')
+    element.setAttribute('style', 'color: red; margin: 10px;');
+    document.body.setAttribute('style','background-color:white')
+}
